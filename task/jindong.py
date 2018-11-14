24338808ec4cc3b3e5484fade6768fdbcedc6e2f
hanhuizhu <hanhuizhuxyl@gmail.com>
2018-11-14 22:07:20 +0800

    update model

---
+++ task/jindong.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+import requests
+from pyquery import PyQuery as pq
+
+types = {
+    # '家用电器'
+}
+
+
+def jindong():
+    try:
+        pass
+    except Exception as e:
+        print(e)
